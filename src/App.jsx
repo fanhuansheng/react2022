e3ae6bd48e20ec95be90beb0a34531199647d2c3
fan1140427762 <1140427762@qq.com>
2022-10-23 15:50:24 +0800

    master测试

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import React, { Component } from 'react'
 import RefMeth from "./01-base/06-ref"
 export default class App extends Component {
     render() {
-        console.log('dev_test')
         return (
             <div>
             {/*<Header/>*/}
